3fb96d59446ac242e6225ae6e78bbd92f71ac8a5
okiktev <oleg.kiktev@gmail.com>
2022-01-10 20:18:43 +0200

    Increased number of chars

--- src/main/java/com/delfin/matrix/Chars.java
+++ src/main/java/com/delfin/matrix/Chars.java
@@ -2,6 +2,7 @@ package com.delfin.matrix;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -11,6 +12,8 @@ class Chars {
 	private static List<Character> DIGITS;
 	private static List<Character> JAPANESE;
 	private static List<Character> LATIN;
+	private static List<Character> UMLAUTS;
+	private static List<Character> USSR_SIGN;
 
 	static List<Character> getDigits() {
 		if (DIGITS == null) {
@@ -38,11 +41,27 @@ class Chars {
 		return LATIN;
 	}
 
+	static List<Character> getUmlauts() {
+		if (UMLAUTS == null) {
+			UMLAUTS = IntStream.range(188, 384).mapToObj(i -> new Character((char) i)).collect(Collectors.toList());
+		}
+		return UMLAUTS;
+	}
+
+	static List<Character> getUSSRSign() {
+		if (USSR_SIGN == null) {
+			USSR_SIGN = Collections.singletonList((char) Integer.parseInt("262D", 16));
+		}
+		return USSR_SIGN;
+	}
+
 	static List<Character> getAll() {
 		List<Character> res = new ArrayList<>(getDigits());
 		res.addAll(getJapanese());
 		res.addAll(getLatin());
+		res.addAll(getUmlauts());
 		return res;
+//		return getUSSRSign();
 	}
 
 }
